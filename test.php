1da0086119278ef5a587490dbfa693809882b5c9
kawtarbenabdelmoumene-ai <kawtarbenabdelmoumene@gmail.com>
2026-02-07 05:29:17 +0100

    Add simple PHP test file to verify server

---
+++ test.php
@@ -0,0 +1,7 @@
+<?php
+// Simple PHP test to verify server is working
+echo "PHP is working!\n";
+echo "PHP Version: " . phpversion() . "\n";
+echo "Server Time: " . date('Y-m-d H:i:s') . "\n";
+echo "Backend Server is Running!\n";
+?>
